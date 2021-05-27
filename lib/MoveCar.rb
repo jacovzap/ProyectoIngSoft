0c43cf7b08a63153c7d863b9fa64329aca75cd85
jacovzap <jacovzap@gmail.com>
2021-05-26 23:35:11 -0400

    pruebas rspec

--- lib/MoveCar.rb
+++ lib/MoveCar.rb
@@ -1,6 +1,50 @@
-def calcularCamino(posicionInicial)
+def calcularCamino(posicionInicial, orientacionInicial, comando)
+
     array = Array.new(5) { Array.new(5) { 0 } }
     numeros = posicionInicial.split(',')
     array = [numeros[0].to_i, numeros[1].to_i]
-    return array; 
+
+    if orientacionInicial == "N"
+        if comando == "D"
+            orientacionFinal = "E"
+        elsif comando == "I"
+            orientacionFinal = "O"
+        elsif comando == "A"
+            array[1] = array[1] + 1
+        end
+    end
+
+    if orientacionInicial == "E"
+        if comando == "D"
+            orientacionFinal = "S"
+        elsif comando == "I"
+            orientacionFinal = "N"
+        elsif comando == "A"
+            array[0] = array[0] - 1
+        end
+    end
+
+    if orientacionInicial == "O"
+        if comando == "D"
+            orientacionFinal = "N"
+        elsif comando == "I"
+            orientacionFinal = "S"
+        elsif comando == "A"
+            array[0] = array[0] + 1
+        end
+    end
+
+    if orientacionInicial == "S"
+        if comando == "D"
+            orientacionFinal = "O"
+        elsif comando == "I"
+            orientacionFinal = "E"
+        elsif comando == "A"
+            array[1] = array[1] - 1
+        end
+    end
+
+
+    return array
+
 end
--- spec/MoveCar_spec.rb
+++ spec/MoveCar_spec.rb
@@ -6,4 +6,44 @@ RSpec.describe "separar correctamente los caracteres introducidos en un string" 
         resultado = calcularCamino(numero)
         expect(resultado).to eq([1,2])
     end
+
+    it "deberia devolver el array [3,2] al introducir el texto 3,2" do
+        numero = "3,2"
+        resultado = calcularCamino(numero)
+        expect(resultado).to eq([3,2])
+    end
+
+    it "deberia devolver la orientacion introducida como texto" do
+        orientacion = "O"
+        resultado = calcularCamino(orientacion)
+        expect(resultado).to eq("O")
+    end
+
+    it "deberia cambiar la orientacion al introducir un comando de girar" do
+        posicionInicial = "1,2"
+        orientacionInicial = "N"
+        comandoOrientacion = "D"
+        resultado = calcularCamino(posicionInicial, orientacionInicial, comandoOrientacion)
+        expect(resultado).to eq("E")
+    end
+
+    it "deberia avanzar una casilla en la direccion correcta al introducir el comando A" do
+        posicionInicial = "1,2"
+        orientacionInicial = "O"
+        comandoAvance = "A"
+        resultado = calcularCamino(posicionInicial, orientacionInicial, comandoAvance)
+        expect(resultado).to eq([2,2])
+    end
+
+    it "deberia avanzar una casilla en la direccion correcta al introducir el comando A" do
+        posicionInicial = "3,3"
+        orientacionInicial = "E"
+        comandoAvance = "A"
+        resultado = calcularCamino(posicionInicial, orientacionInicial, comandoAvance)
+        expect(resultado).to eq([2,3])
+    end
+
+    
+    
+
 end
