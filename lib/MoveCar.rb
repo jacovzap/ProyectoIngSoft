13427caebc2ac24265be6b4a9500a522e9f09766
jacovzap <jacovzap@gmail.com>
2021-05-27 00:48:05 -0400

    primera iteracion rspec

--- lib/MoveCar.rb
+++ lib/MoveCar.rb
@@ -1,50 +1,56 @@
-def calcularCamino(posicionInicial, orientacionInicial, comando)
+def calcularCamino(posicionInicial, orientacionInicial, comandos)
 
     array = Array.new(5) { Array.new(5) { 0 } }
+    
     numeros = posicionInicial.split(',')
-    array = [numeros[0].to_i, numeros[1].to_i]
+    arrayPosicion = [numeros[0].to_i, numeros[1].to_i]
+    arrayComandos = comandos.split('')
 
-    if orientacionInicial == "N"
-        if comando == "D"
-            orientacionFinal = "E"
-        elsif comando == "I"
-            orientacionFinal = "O"
-        elsif comando == "A"
-            array[1] = array[1] + 1
+    arrayComandos.each do |comando| 
+        if orientacionInicial == "N"
+            if comando == "D"
+                orientacionFinal = "E"
+            elsif comando == "I"
+                orientacionFinal = "O"
+            elsif comando == "A"
+                arrayPosicion[1] = arrayPosicion[1] + 1
+            end
+        end
+    
+        if orientacionInicial == "E"
+            if comando == "D"
+                orientacionFinal = "S"
+            elsif comando == "I"
+                orientacionFinal = "N"
+            elsif comando == "A"
+                arrayPosicion[0] = arrayPosicion[0] - 1
+            end
+        end
+    
+        if orientacionInicial == "O"
+            if comando == "D"
+                orientacionFinal = "N"
+            elsif comando == "I"
+                orientacionFinal = "S"
+            elsif comando == "A"
+                arrayPosicion[0] = arrayPosicion[0] + 1
+            end
+        end
+    
+        if orientacionInicial == "S"
+            if comando == "D"
+                orientacionFinal = "O"
+            elsif comando == "I"
+                orientacionFinal = "E"
+            elsif comando == "A"
+                arrayPosicion[1] = arrayPosicion[1] - 1
+            end
+        end
+
+        if comando != "A"
+            orientacionInicial = orientacionFinal
         end
     end
-
-    if orientacionInicial == "E"
-        if comando == "D"
-            orientacionFinal = "S"
-        elsif comando == "I"
-            orientacionFinal = "N"
-        elsif comando == "A"
-            array[0] = array[0] - 1
-        end
-    end
-
-    if orientacionInicial == "O"
-        if comando == "D"
-            orientacionFinal = "N"
-        elsif comando == "I"
-            orientacionFinal = "S"
-        elsif comando == "A"
-            array[0] = array[0] + 1
-        end
-    end
-
-    if orientacionInicial == "S"
-        if comando == "D"
-            orientacionFinal = "O"
-        elsif comando == "I"
-            orientacionFinal = "E"
-        elsif comando == "A"
-            array[1] = array[1] - 1
-        end
-    end
-
-
-    return array
+    return arrayPosicion
 
 end
--- spec/MoveCar_spec.rb
+++ spec/MoveCar_spec.rb
@@ -13,13 +13,13 @@ RSpec.describe "separar correctamente los caracteres introducidos en un string" 
         expect(resultado).to eq([3,2])
     end
 
-    it "deberia devolver la orientacion introducida como texto" do
+    it "deberia devolver la orientacion O como texto introducida la orientacion 0" do
         orientacion = "O"
         resultado = calcularCamino(orientacion)
         expect(resultado).to eq("O")
     end
 
-    it "deberia cambiar la orientacion al introducir un comando de girar" do
+    it "deberia cambiar la orientacion N a E la introducir un comando de girar D" do
         posicionInicial = "1,2"
         orientacionInicial = "N"
         comandoOrientacion = "D"
@@ -27,7 +27,7 @@ RSpec.describe "separar correctamente los caracteres introducidos en un string" 
         expect(resultado).to eq("E")
     end
 
-    it "deberia avanzar una casilla en la direccion correcta al introducir el comando A" do
+    it "deberia avanzar devoler [2,2] introducida la posicion 1,2, orientacion O y comando A" do
         posicionInicial = "1,2"
         orientacionInicial = "O"
         comandoAvance = "A"
@@ -35,7 +35,7 @@ RSpec.describe "separar correctamente los caracteres introducidos en un string" 
         expect(resultado).to eq([2,2])
     end
 
-    it "deberia avanzar una casilla en la direccion correcta al introducir el comando A" do
+    it "deberia avanzar devoler [2,3] introducida la posicion 3,3 orientacion E y comando A" do
         posicionInicial = "3,3"
         orientacionInicial = "E"
         comandoAvance = "A"
@@ -43,7 +43,29 @@ RSpec.describe "separar correctamente los caracteres introducidos en un string" 
         expect(resultado).to eq([2,3])
     end
 
-    
-    
+    it "deberia devolver el array de instrucciones ['I','A','I','A','I','A','A'] al introducir como texto el comando IAIAIAA" do
+        posicionInicial = "3,3"
+        orientacionInicial = "E"
+        comandoAvance = "IAIAIAA"
+        resultado = calcularCamino(posicionInicial, orientacionInicial, comandoAvance)
+        expect(resultado).to eq(["I","A","I","A","I","A","A"])
+    end
+
+    it "deberia devolver [1,3] al introducir como texto el comando IAIAIAIAA, la posicion inicial 1,2 y la orientacion inicial N" do
+        posicionInicial = "1,2"
+        orientacionInicial = "N"
+        comandoAvance = "IAIAIAIAA"
+        resultado = calcularCamino(posicionInicial, orientacionInicial, comandoAvance)
+        expect(resultado).to eq([1,3])
+    end
+
+    it "deberia devolver [1,1] al introducir como texto el comando AADAADADDA, la posicion inicial 3,3 y la orientacion inicial E" do
+        posicionInicial = "3,3"
+        orientacionInicial = "E"
+        comandoAvance = "AADAADADDA"
+        resultado = calcularCamino(posicionInicial, orientacionInicial, comandoAvance)
+        expect(resultado).to eq([1, 1])
+    end
+
 
 end
